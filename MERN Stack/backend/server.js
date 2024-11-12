b5093b008b36115f727be16eafb2e59f8dd57638
Naveen-Dilsh <naveentype2001@gmail.com>
2024-11-13 01:55:29 +0530

    3 , 4  Videos
    Set Mongodb

--- MERN Stack/backend/server.js
+++ MERN Stack/backend/server.js
@@ -1,9 +1,15 @@
 require("dotenv").config()
+
 const express = require('express');
+const mongoose = require("mongoose")
+const workoutRoutes = require("./routes/workout")
 
 
 // express app
 const app = express()
+
+//midleware
+app.use(express.json())
 
 app.use((req, res, next) =>{
     console.log(req.path,req.method)
@@ -11,11 +17,22 @@ app.use((req, res, next) =>{
 })
 
 //routes
-app.get("/",(req,res) =>{
-    res.json({mssg:"welcome to the app"})
-})
+app.use('/api/workouts',workoutRoutes)
+// app.get("/",(req,res) =>{
+//     res.json({mssg:"welcome to the app"})
+// })
 
-//listen for requests
-app.listen(process.env.PORT, ()=>{
-    console.log("listening on port 4000!!!")
-})
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=> {
+        //listen for requests
+        app.listen(process.env.PORT, ()=>{
+        console.log("connected to listening on port", process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
